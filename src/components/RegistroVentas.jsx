dbaa01b59c03a6e64afb94266408d5e6039800b1
gercho12 <alangerscho@gmail.com>
2025-09-01 16:12:39 -0300

    ip

--- src/components/RegistroVentas.jsx
+++ src/components/RegistroVentas.jsx
@@ -18,7 +18,7 @@ export default function RegistroVentas() {
   const codigoInputRef = useRef(null);
 
   // Configuración de la API
-  const API_BASE = 'http://localhost:3001';
+  const API_BASE = 'http://18.119.160.177:3001';
 
   // Efecto para manejar eventos globales de teclado y cargar historial
   useEffect(() => {
